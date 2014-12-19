d29ed68376a42132a2c45a6f35d18bf113d107f7
Joe W <joe.wright@jestercom.com>
2014-12-19 11:09:54 -0500

    init

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var fs = require('fs');
+module.exports = function(grunt) {
+	// Project configuration.
+	var entpkgpath = path.join(__dirname, 'node-ent/package.json');
+	var pkgjson = require(entpkgpath);
+	if (!pkgjson.guid) {
+		console.log('writing guid to node-ent/package.json...');
+		pkgjson.guid = '361b0272-c428-d5b3-cd23-919fdbc2fe35';
+	}
+	fs.writeFileSync(entpkgpath, JSON.stringify(pkgjson));
+
+	grunt.initConfig({
+		titaniumifier: {
+			"module": {
+				files: {
+					'./': path.join(__dirname, 'node-ent')
+				},
+				options: {
+					guid: '51d71d1d-9b0e-ad82-41f4-87fe0eb89df3',
+					// You can create a CommonJS module **without** the dependencies bundled
+					bare: true
+				}
+			}
+		},
+	});
+	grunt.loadNpmTasks('grunt-titaniumifier');
+	grunt.registerTask('titaniumify', ['titaniumifier']);
+};
